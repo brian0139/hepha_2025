9ca54c8cb5220a461d838847cb4b5fca56aa1a3d
Brian Wang <potatothepotato061@gmail.com>
2025-10-05 16:38:42 -0700

    added packages for everyone

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Shannon/ShanPath.java
@@ -0,0 +1,27 @@
+package com.example.meepmeeptesting.Shannon;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class ShanPath {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(750);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-60, 0, 0))
+                // add your own code now OR ELSE!!!!!
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
