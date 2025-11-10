52de55928ab47739e1678989a8e99c5c75d72278
stanley <stanleyzihanfan@gmail.com>
2025-11-09 17:33:27 -0800

    fixed drivetrain code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shannon/driveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shannon/driveTrain.java
@@ -13,22 +13,20 @@ import java.lang.Math;
 
 public class driveTrain extends LinearOpMode{
     // drivetrain wheel motor declaration
-    private DcMotor wheel_0=null;
-    private DcMotor wheel_1=null;
-    private DcMotor wheel_2=null;
-    private DcMotor wheel_3=null;
+    private DcMotor leftFront=null;
+    private DcMotor leftBack=null;
+    private DcMotor rightFront=null;
+    private DcMotor rightBack=null;
 
     //main loop
     @Override
     public void runOpMode() {
         //initiate drivetrain motors
-        wheel_0   = hardwareMap.get(DcMotor.class, "wheel_0");
-        wheel_1    = hardwareMap.get(DcMotor.class, "wheel_1");
-        wheel_2   = hardwareMap.get(DcMotor.class, "wheel_2");
-        wheel_3   = hardwareMap.get(DcMotor.class, "wheel_3");
+        leftFront   = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack    = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront   = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack   = hardwareMap.get(DcMotor.class, "rightBack");
 
-        wheel_2.setDirection(DcMotor.Direction.REVERSE);
-        wheel_3.setDirection(DcMotor.Direction.REVERSE);
         //telemetry message to signify robot waiting
         telemetry.addLine("Robot Ready.");
         telemetry.update();
@@ -39,26 +37,28 @@ public class driveTrain extends LinearOpMode{
             //below is drivetrain
             // Mecanum drive is controlled with three axes: drive (front-and-back),
             // strafe (left-and-right), and twist (rotating the whole chassis).
-            double drive  = gamepad1.left_stick_y*0.7;
+            //Default:0.7
+            double drive  = gamepad1.right_stick_y*0.7;
             final double strafe_speed=0.5;
-            double strafe = -gamepad1.left_stick_x*0.5;
+            //Default:0.5
+            double strafe = -gamepad1.right_stick_x*0.5;
             if (gamepad1.dpad_left){
                 strafe=strafe_speed;
             }
             else if (gamepad1.dpad_right){
                 strafe=-strafe_speed;
             }
-            double twist  = -gamepad1.right_stick_x;
+            double twist  = -gamepad1.left_stick_x*0.5;
             telemetry.addData("drive: ", drive);
             telemetry.addData("strafe: ", strafe);
             telemetry.addData("twist: ", twist);
             telemetry.update();
 
             double[] speeds = {
-                    (drive + strafe + twist), //forward-left motor(wheel_0)
-                    (drive - strafe - twist), //forward-right motor(wheel_1)
-                    (drive - strafe + twist), //back-left motor(wheel_2)
-                    (drive + strafe - twist)  //back-right motor(wheel_3)
+                    (drive - strafe + twist), //forward-left motor
+                    (drive + strafe + twist), //forward-right motor
+                    (-drive - strafe + twist), //back-left motor
+                    (-drive + strafe + twist)  //back-right motor
             };
 
             // Loop through all values in the speeds[] array and find the greatest
@@ -75,10 +75,10 @@ public class driveTrain extends LinearOpMode{
             }
 
             // apply the calculated values to the motors.
-            wheel_0.setPower(speeds[0]);
-            wheel_3.setPower(speeds[1]);
-            wheel_1.setPower(speeds[2]);
-            wheel_2.setPower(speeds[3]);
+            leftFront.setPower(speeds[0]);
+            rightBack.setPower(speeds[1]);
+            leftBack.setPower(speeds[2]);
+            rightFront.setPower(speeds[3]);
         }
     }
 }
