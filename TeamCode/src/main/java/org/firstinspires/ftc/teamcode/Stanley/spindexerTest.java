a61712bb695bf46ddea171363f3a3ecd77a69efa
stanley <stanleyzihanfan@gmail.com>
2025-11-29 18:47:49 -0800

    save commit(full hood test, WIP outtake auto hood angle setting+relevant changes in outtakeTest due to this change)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/spindexerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/spindexerTest.java
@@ -16,27 +16,27 @@ public class spindexerTest extends LinearOpMode{
         spindexer=hardwareMap.get(Servo.class,"spindexerServo");
         transfer=hardwareMap.get(Servo.class,"transferServo");
         double spindexerpos=0;
-        double transferPos=0.3;
+        double transferPos=0.33;
         double spindexerDialation=0.001;
         double transferDialation=0.001;
         double[] outtakeslots = {60.0/360, 180.0/360, 300.0/360};
-        double[] transferpositions ={0.1,0.3};
+        double[] transferpositions ={0.1,0.33};
         int outtakePos=0;
         int transferListPos=0;
+        boolean lasty=false;
+        boolean lastx=false;
         //telemetry message to signify robot waiting
         telemetry.addLine("Robot Ready.");
         telemetry.update();
         //wait for driver to press play
         waitForStart();
-        boolean lasty=false;
-        boolean lastx=false;
         //repeat untill opmode ends
-        //TODO:Limit transfer to 0.1<=x<=0.30
+        //TODO:Limit transfer to 0.1<=x<=0.33
         while (opModeIsActive()){
             if (spindexerpos-gamepad1.left_stick_x*spindexerDialation>=0 && spindexerpos-gamepad1.left_stick_x*spindexerDialation<=1){
                 spindexerpos-=gamepad1.left_stick_x*spindexerDialation;
             }
-            if (transferPos+gamepad1.right_stick_y*transferDialation>=0.1 && transferPos+gamepad1.right_stick_y*transferDialation<=0.30){
+            if (transferPos+gamepad1.right_stick_y*transferDialation>=transferpositions[0] && transferPos+gamepad1.right_stick_y*transferDialation<=transferpositions[1]){
                 transferPos+=gamepad1.right_stick_y*transferDialation;
             }
             if (gamepad1.y && !lasty){
