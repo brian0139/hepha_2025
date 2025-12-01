c0c01c4073a86cb1a185544a29477fb6502f6d5b
Brian Wang <potatothepotato061@gmail.com>
2025-11-30 20:50:57 -0800

    update commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/spindexerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/spindexerTest.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode.Stanley;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import java.lang.Math;
 @TeleOp
 
 public class spindexerTest extends LinearOpMode{
@@ -16,27 +22,24 @@ public class spindexerTest extends LinearOpMode{
         spindexer=hardwareMap.get(Servo.class,"spindexerServo");
         transfer=hardwareMap.get(Servo.class,"transferServo");
         double spindexerpos=0;
-        double transferPos=0.33;
-        double spindexerDialation=0.001;
-        double transferDialation=0.001;
-        double[] outtakeslots = {0.26,0.65,1};
-        double[] transferpositions ={0.6,0.9};
+        double transferPos=0.3;
+        double spindexerDialation=0.0001;
+        double transferDialation=0.0001;
+        double[] outtakeslots = {60.0/360, 180.0/360, 300.0/360};
         int outtakePos=0;
-        int transferListPos=0;
-        boolean lasty=false;
-        boolean lastx=false;
         //telemetry message to signify robot waiting
         telemetry.addLine("Robot Ready.");
         telemetry.update();
         //wait for driver to press play
         waitForStart();
+        boolean lasty=false;
         //repeat untill opmode ends
-        //TODO:Limit transfer to 0.1<=x<=0.33
+        //TODO:Limit transfer to 0.1<=x<=0.30
         while (opModeIsActive()){
             if (spindexerpos-gamepad1.left_stick_x*spindexerDialation>=0 && spindexerpos-gamepad1.left_stick_x*spindexerDialation<=1){
                 spindexerpos-=gamepad1.left_stick_x*spindexerDialation;
             }
-            if (transferPos+gamepad1.right_stick_y*transferDialation>=transferpositions[0] && transferPos+gamepad1.right_stick_y*transferDialation<=transferpositions[1]){
+            if (transferPos+gamepad1.right_stick_y*transferDialation>=0.1 && transferPos+gamepad1.right_stick_y*transferDialation<=0.30){
                 transferPos+=gamepad1.right_stick_y*transferDialation;
             }
             if (gamepad1.y && !lasty){
@@ -46,18 +49,10 @@ public class spindexerTest extends LinearOpMode{
             }else if (!gamepad1.y){
                 lasty=false;
             }
-            if (gamepad1.x && !lastx){
-                lastx=true;
-                transferListPos++;
-                transferPos= transferpositions[transferListPos%2];
-            }else if (!gamepad1.x){
-                lastx=false;
-            }
             spindexer.setPosition(spindexerpos);
             transfer.setPosition(transferPos);
             telemetry.addData("spindexerPos:",spindexerpos);
             telemetry.addData("transferPos:",transferPos);
-            telemetry.addLine("transferListPos:"+transferListPos+"("+ transferpositions[transferListPos%2]+")");
             telemetry.addLine("outtakePos:"+outtakePos+"("+outtakeslots[outtakePos%3]+")");
             telemetry.update();
             
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.TankDrive;
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(53, 58, Math.toRadians(270));
+        Pose2d beginPose = new Pose2d(0, 0, 0);
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
@@ -19,8 +19,8 @@ public final class SplineTest extends LinearOpMode {
 
             Actions.runBlocking(
                 drive.actionBuilder(beginPose)
-                        .splineTo(new Vector2d(20, 20),Math.toRadians(90))
-                        .splineTo(new Vector2d(38, 50),Math.toRadians(0))
+                        .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                        .splineTo(new Vector2d(0, 60), Math.PI)
                         .build());
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, beginPose);
