4aad627b3beee85c65f6b5c4c2eb3456ccb313b6
stanleyzihanfan <stanleyzihanfan@gmail.com>
2025-12-02 17:53:22 -0800

    partially finished auto meepmeep path, created file for auton path, changed transfer servo lowered position from 0.9 to 0.85

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/autonPath/AutonRedPath.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Stanley.autonPath;
+
+public class AutonRedPath {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/drivetrainMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/drivetrainMain.java
@@ -33,7 +33,7 @@ public class drivetrainMain extends LinearOpMode{
     boolean spindexerPosition=false;
     double[] outtakeslots = {0.26,0.65,1};
     double[] intakeslots = {0.05,0.44,0.83};
-    double[] transferpositions ={0.6,0.9};
+    double[] transferpositions ={0.6,0.85};
     int outtakepos=0;
     int intakepos=0;
     boolean pasty=false;
