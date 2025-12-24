b0e23a862cebe251ca4101e1230fa4524d0c3e26
Brian Wang <potatothepotato061@gmail.com>
2025-12-24 12:13:17 -0800

    spindexer color class with spin to motif, and testing class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Brian/spindexerColorTest.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.Brian;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Alvin.colorSensor;
+import java.util.Vector;
+
+@TeleOp
+
+public class spindexerColorTest extends LinearOpMode{
+    CRServo spindexer;
+    colorSensor colorsensoroperator;
+    spindexerColor spindexercolor;
+
+    //main loop
+    @Override
+    public void runOpMode() {
+        //initiate drivetrain motors
+        spindexer=hardwareMap.get(CRServo.class,"spindexerServo");
+        colorsensoroperator=new colorSensor(hardwareMap,"colorSensor");
+        spindexercolor=new spindexerColor(spindexer);
+        double spindexerpower=0.5;
+        double stoppower=0.001;
+        //telemetry message to signify robot waiting
+        telemetry.addLine("Robot Ready.");
+        telemetry.update();
+        Vector<String> detections=new Vector<>();
+        //wait for driver to press play
+        waitForStart();
+        //repeat until opmode ends
+        while (opModeIsActive()){
+            if (gamepad1.yWasPressed()) spindexercolor.spinToMotif(spindexerpower);
+//            if (gamepad1.dpadUpWasPressed())
+//            if (gamepad1.dpadDownWasPressed()
+            int detected = colorsensoroperator.getDetected();
+
+            String result;
+            if (detected == 1) {
+                result = "GREEN";
+            } else if (detected == 2) {
+                result = "PURPLE";
+            } else {
+                result = "NONE";
+            }
+
+            telemetry.addData("Detected", result);
+
+            float[] hsv = colorsensoroperator.readHSV();
+            telemetry.addData("Hue", hsv[0]);
+            telemetry.addData("Sat", hsv[1]);
+            telemetry.addData("Val", hsv[2]);
+
+            if (result.equals("GREEN")) spindexer.setPower(stoppower);
+//            if (colorsensoroperator.getDetected()==1) detections.add("Green");
+//            if (colorsensoroperator.getDetected()==2) detections.add("Purple");
+//            String tmp="";
+//            for (String i : detections){
+//                tmp=tmp+i+", ";
+//            }
+//            telemetry.addLine(tmp);
+            telemetry.addData("spindexer Power",spindexerpower);
+            telemetry.update();
+        }
+    }
+}
