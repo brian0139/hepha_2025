65d6a44aac4fe935f733c1393591eed73c298cda
stanleyzihanfan <stanleyzihanfan@gmail.com>
2025-12-29 14:45:58 -0800

    WARNING: Updated gradle to V8.13.1, may cause incompatibilities testing analog input

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/testingOpmodes/analoginputtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/testingOpmodes/analoginputtest.java
@@ -1,22 +1,31 @@
 package org.firstinspires.ftc.teamcode.Stanley.testingOpmodes;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp
 public class analoginputtest extends LinearOpMode {
     AnalogInput input=null;
     CRServo servo=null;
+    FtcDashboard dashboard=null;
+    Telemetry dashboardTelemetry=null;
     @Override
     public void runOpMode(){
         input=hardwareMap.get(AnalogInput.class,"hoodAnalog");
         servo=hardwareMap.get(CRServo.class,"hoodServo");
+        dashboard=FtcDashboard.getInstance();
+        dashboardTelemetry=dashboard.getTelemetry();
         waitForStart();
         while(opModeIsActive()){
             servo.setPower(-gamepad1.left_stick_y);
             telemetry.addData("Voltage",input.getVoltage());
+            dashboardTelemetry.addData("Voltage",input.getVoltage());
+            dashboardTelemetry.update();
             telemetry.update();
         }
     }
