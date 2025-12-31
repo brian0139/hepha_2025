3879971a46aeaf31e71c9a0f19bfe18d72ab23e6
stanleyzihanfan <stanleyzihanfan@gmail.com>
2025-12-31 12:11:16 -0800

    save commit(Working on hood tuning opmode)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/testingOpmodes/outtakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/testingOpmodes/outtakeTest.java
@@ -3,6 +3,7 @@ import org.firstinspires.ftc.teamcode.Stanley.finalizedClasses.outtakeV2;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -20,19 +21,15 @@ public class outtakeTest extends LinearOpMode{
     //main loop
     @Override
     public void runOpMode() {
-        double servoRPM=50;
-        double msPerRotation=60000/servoRPM;
-        double hoodAngle=30;
         //outtake instance
         hoodServo=hardwareMap.get(CRServo.class,"hoodServo");
+        AnalogInput hoodSensor=hardwareMap.get(AnalogInput.class,"hoodAnalog");
         flywheelDrive=hardwareMap.get(DcMotorEx.class,"flywheel");
         flywheelDriveR=hardwareMap.get(DcMotorEx.class,"flywheelR");
         transfer=hardwareMap.get(DcMotor.class,"par1");
         ElapsedTime timer=new ElapsedTime();
         //TODO:add hood Sensor
-        outtakeV2 outtakeOperator = new outtakeV2(hardwareMap, flywheelDrive, flywheelDriveR,"Red",null,null,null,null,hoodServo,null,transfer,false);
-        outtakeOperator.hoodAngle=hoodAngle;
-        outtakeOperator.savehoodAngle=hoodAngle;
+        outtakeV2 outtakeOperator = new outtakeV2(hardwareMap, flywheelDrive, flywheelDriveR,"Red",null,null,null,null,hoodServo,hoodSensor,transfer,false);
 
         //telemetry message to signify robot waiting
         telemetry.addLine("Robot Ready.");
@@ -40,26 +37,11 @@ public class outtakeTest extends LinearOpMode{
         //wait for driver to press play
         waitForStart();
         while (opModeIsActive()){
-            hoodAngle-=gamepad1.left_stick_y*0.001;
-            hoodServo.setPower(gamepad1.right_stick_y);
-            if (gamepad1.rightBumperWasPressed()){
-                timer.reset();
-                while (timer.milliseconds()<msPerRotation) hoodServo.setPower(1);
-                hoodServo.setPower(0);
+            if (gamepad1.rightBumperWasPressed()) {
+                while(hoodSensor.getVoltage()<=3.28) hoodServo.setPower(0.2);
             }
-            if (gamepad1.leftBumperWasPressed()){
-                timer.reset();
-                while (timer.milliseconds()<msPerRotation) hoodServo.setPower(-1);
-                hoodServo.setPower(0);
-            }
-            if (gamepad1.yWasPressed()){
-                outtakeOperator.setHood(hoodAngle);
-            }
-            telemetry.addData("Running hood",outtakeOperator.initializingHood);
-            telemetry.addData("targethoodangle",hoodAngle);
-            if (outtakeOperator.initializingHood) {
-                outtakeOperator.updateHoodAngle();
-                telemetry.addData("Expected hood angle", outtakeOperator.hoodAngle);
+            if (gamepad1.leftBumperWasPressed()) {
+                while(hoodSensor.getVoltage()>=0.2) hoodServo.setPower(-0.2);
             }
             telemetry.update();
         }
