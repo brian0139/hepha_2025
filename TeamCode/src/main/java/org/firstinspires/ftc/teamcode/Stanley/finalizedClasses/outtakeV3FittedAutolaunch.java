e6f78525c33a19603b321811111803ca2dac7c5f
stanleyzihanfan <stanleyzihanfan@gmail.com>
2026-01-20 22:12:43 -0800

    save commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/finalizedClasses/outtakeV3FittedAutolaunch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stanley/finalizedClasses/outtakeV3FittedAutolaunch.java
@@ -295,12 +295,12 @@ public class outtakeV3FittedAutolaunch {
         return (hoodEncoder.getCurrentPosition()+encoderOffset >= targetTicks - epsilon) && (hoodEncoder.getCurrentPosition()+encoderOffset <= targetTicks + epsilon);
     }
 
-    public boolean setHoodEncoder(double ticks){
-        double epsilon=40;
-        double power=hoodPID.update(ticks-hoodEncoder.getCurrentPosition()+encoderOffset);
-        hoodServo.setPower(-power);
-        return (hoodEncoder.getCurrentPosition()+encoderOffset >= ticks - epsilon) && (hoodEncoder.getCurrentPosition()+encoderOffset <= ticks + epsilon);
-    }
+//    public boolean setHoodEncoder(double ticks){
+//        double epsilon=40;
+//        double power=hoodPID.update(ticks-hoodEncoder.getCurrentPosition()+encoderOffset);
+//        hoodServo.setPower(-power);
+//        return (hoodEncoder.getCurrentPosition()+encoderOffset >= ticks - epsilon) && (hoodEncoder.getCurrentPosition()+encoderOffset <= ticks + epsilon);
+//    }
 
     /**
      * Set the hood angle to a specific degree
